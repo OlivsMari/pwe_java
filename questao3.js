e9b05992fec8eb88a52d6fab10dff562a5171b09
OlivsMari <160743010+OlivsMari@users.noreply.github.com>
2024-10-30 13:28:45 +0000

    questoes

---
+++ questao3.js
@@ -0,0 +1,14 @@
+
+
+var nomeAluno = "Marina de Oliveira";
+var nota1 = 7;
+var nota2 = 8.5;
+var pesoNota1 = nota1 * 0.6;
+var pesoNota2 = nota2 * 0.4;
+var notaTotal = (pesoNota1 + pesoNota2).toFixed(2);
+
+
+alert("Nome do aluno: " + nomeAluno + "\nNota 1: " + nota1 + "\nNota 2: " + nota2);
+
+
+alert("Nota total do aluno: " + notaTotal;)
